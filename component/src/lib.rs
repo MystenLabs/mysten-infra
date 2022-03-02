a16297edd18522c3e7d4254e5fff96eba960160b
Laura Makdah <laura@mystenlabs.com>
2022-03-02 15:27:54 -0800

    use trait and implement join, terminate, restart

--- component/src/lib.rs
+++ component/src/lib.rs
@@ -1,58 +1,60 @@
-use std::future;
-use tokio::{sync::mpsc::{channel, Receiver, }};
-use std::future::Future;
-use tokio::sync::oneshot::Sender;
+use futures::future;
+use tokio::{sync::mpsc::{channel, Sender, Receiver}};
+use tokio::sync::oneshot::{channel as oneshotChannel, Sender as oneshotSender, Receiver as oneshotReceiver};
+use tokio::time::{Duration, timeout};
 
 type IrrecoverableError = std::io::Error;
+type TokioJoinHande = tokio::task::JoinHandle<()>;
 
+pub trait Manageable {
+    fn start(&self, _: Sender<IrrecoverableError>, _: oneshotReceiver<()>) -> future::BoxFuture<'static, ()>;
+
+}
 
 pub struct Component {
-    complete: Receiver<IrrecoverableError>,
-    handle: tokio::task::JoinHandle<Result<(), Err()>>,
+    cancel_signal: oneshotSender<()>,
+    panic_signal: Receiver<IrrecoverableError>,
+    join_handle: TokioJoinHande,
+    start_fn : Box<dyn Manageable>,
 }
 
-pub struct ComponentManager {
-    handles: Vec<Component>,
-}
-
-impl ComponentManager {
-    fn new() -> ComponentManager {
-        let mut handles = Vec::new();
-        ComponentManager{handles}
-    }
-
-    fn spawn<F>(&mut self, f:F) -> &Component //  input param: function that takes a sender as a param and returns a future
-    where F: Fn(Sender<IrrecoverableError>) -> Future<Output = ()>
+impl Component {
+    pub async fn spawn<M>(&mut self, starter: M)
+    where M: Manageable + Send + Copy + 'static
     {
-        let (sender, mut receiver) = channel(10);
-        let wrapped_handle = tokio::spawn(f(sender));
-        let mut component = Component{complete: receiver, handle: wrapped_handle};
-        self.handles.push(component);
-        &component
+        let (panic_sender, panic_receiver) = channel(10);
+        let (cancel_sender, cancel_receiver) = oneshotChannel();
+        let wrapped_handle = tokio::spawn(starter.start(panic_sender, cancel_receiver));
+        self.panic_signal = panic_receiver;
+        self.cancel_signal = cancel_sender;
+        self.join_handle = wrapped_handle;
+        self.start_fn = Box::new(starter);
     }
 
-    // async fn run(&self) {
-    //     tokio::select! {
-    //         Some(result) = self.message_receivers.recv() => {
-    //             // log and restart
-    //         }
-    //         _ = self.futures.iter_mut() => {
-    //             //restart
-    //         }
-    //
-    //     }
-        // let mut select_all = select_all(&self.message_receivers);
-        // loop {
-        //     tokio::select! {
-        //         msg = select_all.next() => {
-        //             // log and restart
-        //         }
-        //         _ = self.futures.select_next_some() => {
-        //             // restart
-        //         }
-        //     }
-        // }
-    // }
+    pub async fn join(self) -> Result<(), std::io::Error> {
+        self.join_handle.await?;
+        Ok(())
+    }
+
+    pub async fn terminate(mut self) -> Result<(), std::io::Error> {
+        self.cancel_signal.send(()).unwrap();
+
+        if let Err(_) = timeout(Duration::from_secs(2), &mut self.join_handle).await {
+            self.join_handle.abort();
+        }
+
+        Ok(())
+    }
+
+    pub async fn restart(mut self) {
+        let (panic_sender, panic_receiver) = channel(10);
+        let (cancel_sender, cancel_receiver) = oneshotChannel();
+        let wrapped_handle = tokio::spawn(self.start_fn.start(panic_sender, cancel_receiver));
+        self.panic_signal = panic_receiver;
+        self.cancel_signal = cancel_sender;
+        self.join_handle = wrapped_handle;
+    }
+
 }
 
 
