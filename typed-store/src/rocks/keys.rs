938d2c87856b59061d7ab00f3823c1d773f265b2
François Garillot <francois@garillot.net>
2022-03-06 08:07:11 -0500

    feat: expose rocksdb's seek_to_last as skip_to_last

--- typed-store/src/rocks/keys.rs
+++ typed-store/src/rocks/keys.rs
@@ -62,4 +62,11 @@ impl<'a, K: Serialize> Keys<'a, K> {
         self.db_iter.seek_for_prev(config.serialize(key)?);
         Ok(self)
     }
+
+    /// Seeks to the last key in the database (at this column family).
+    ///
+    pub fn skip_to_last(mut self) -> Self {
+        self.db_iter.seek_to_last();
+        self
+    }
 }
