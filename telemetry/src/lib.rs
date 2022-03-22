4f0f28435b61777aa2a117efbd344eef6ec6ac5d
Evan Chan <velvia@gmail.com>
2022-03-21 21:38:15 -0700

    Add panic logging hook;  Add test and cargo fmt

--- telemetry/src/lib.rs
+++ telemetry/src/lib.rs
@@ -47,6 +47,33 @@ fn get_output(config: &TelemetryConfig) -> NonBlocking {
     }
 }
 
+// NOTE: this function is copied from tracing's panic_hook example
+fn set_panic_hook() {
+    // Set a panic hook that records the panic as a `tracing` event at the
+    // `ERROR` verbosity level.
+    //
+    // If we are currently in a span when the panic occurred, the logged event
+    // will include the current span, allowing the context in which the panic
+    // occurred to be recorded.
+    std::panic::set_hook(Box::new(|panic| {
+        // If the panic has a source location, record it as structured fields.
+        if let Some(location) = panic.location() {
+            // On nightly Rust, where the `PanicInfo` type also exposes a
+            // `message()` method returning just the message, we could record
+            // just the message instead of the entire `fmt::Display`
+            // implementation, avoiding the duplicated location
+            tracing::error!(
+                message = %panic,
+                panic.file = location.file(),
+                panic.line = location.line(),
+                panic.column = location.column(),
+            );
+        } else {
+            tracing::error!(message = %panic);
+        }
+    }));
+}
+
 /// Initialize telemetry subscribers based on TelemetryConfig
 pub fn init(config: TelemetryConfig) {
     // TODO: reorganize different telemetry options so they can use the same registry
@@ -54,14 +81,10 @@ pub fn init(config: TelemetryConfig) {
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let nb_output = get_output(&config);
 
-    // See [[dev-docs/observability.md]] for more information on span logging.
     if config.json_log_output {
         // See https://www.lpalmieri.com/posts/2020-09-27-zero-to-production-4-are-we-observable-yet/#5-7-tracing-bunyan-formatter
         // Also Bunyan layer addes JSON logging for tracing spans with duration information
-        let formatting_layer = BunyanFormattingLayer::new(
-            config.service_name,
-            nb_output,
-        );
+        let formatting_layer = BunyanFormattingLayer::new(config.service_name, nb_output);
         // The `with` method is provided by `SubscriberExt`, an extension
         // trait for `Subscriber` exposed by `tracing_subscriber`
         let subscriber = Registry::default()
@@ -77,14 +100,13 @@ pub fn init(config: TelemetryConfig) {
         console_subscriber::init();
     } else {
         // Output to file or to stdout with ANSI colors
-        let fmt_layer = fmt::layer().with_ansi(config.log_file.is_none()).with_writer(nb_output);
+        let fmt_layer = fmt::layer()
+            .with_ansi(config.log_file.is_none())
+            .with_writer(nb_output);
 
         // Standard env filter (RUST_LOG) with standard formatter
-        let subscriber = Registry::default()
-            .with(env_filter)
-            .with(fmt_layer);
+        let subscriber = Registry::default().with(env_filter).with(fmt_layer);
 
-        // We assume you would not enable both SUI_JSON_SPAN_LOGS and open telemetry at same time, but who knows?
         if config.enable_tracing {
             // Install a tracer to send traces to Jaeger.  Batching for better performance.
             let tracer = opentelemetry_jaeger::new_pipeline()
@@ -101,8 +123,31 @@ pub fn init(config: TelemetryConfig) {
             global::set_text_map_propagator(TraceContextPropagator::new());
 
             set_global_default(subscriber.with(telemetry)).expect("Failed to set subscriber");
+            info!("Jaeger tracing initialized");
         } else {
             set_global_default(subscriber).expect("Failed to set subscriber");
         }
+    }
+
+    set_panic_hook();
+}
+
+mod tests {
+    use super::*;
+    use tracing::{debug, info, warn};
+
+    #[test]
+    #[should_panic]
+    fn test_telemetry_init() {
+        let config = TelemetryConfig {
+            service_name: "my_app".into(),
+            ..Default::default()
+        };
+        init(config);
+
+        info!(a = 1, "This will be INFO.");
+        debug!(a = 2, "This will be DEBUG.");
+        warn!(a = 3, "This will be WARNING.");
+        panic!("This should cause error logs to be printed out!");
     }
 }
