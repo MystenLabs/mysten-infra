85b8bc77e9398942373a0c31d56eea368c7d0a25
Evan Chan <velvia@gmail.com>
2022-03-22 20:57:41 -0700

    Use cargo features for tokio-console, json/bunyan, and jaeger

--- telemetry/src/lib.rs
+++ telemetry/src/lib.rs
@@ -4,6 +4,7 @@
 //! This is a library for common Tokio Tracing subscribers, such as for Jaeger.
 //!
 //! The subscribers are configured using TelemetryConfig passed into the `init()` method.
+//! A panic hook is also installed so that panics/expects result in scoped error logging.
 //!
 //! Getting started is easy:
 //! ```
@@ -14,14 +15,24 @@
 //! // Important! Need to keep the guard and not drop until program terminates
 //! let guard = telemetry::init(config);
 //! ```
+//!
+//! ## Features
+//! - `jaeger` - this feature is enabled by default as it enables jaeger tracing
+//! - `json` - Bunyan formatter - JSON log output, optional
+//! - `tokio-console` - [Tokio-console](https://github.com/tokio-rs/console) subscriber, optional
 
+#[cfg(feature = "jaeger")]
 use opentelemetry::global;
+#[cfg(feature = "jaeger")]
 use opentelemetry::sdk::propagation::TraceContextPropagator;
+
 use tracing::info;
 use tracing::subscriber::set_global_default;
 use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter, Registry};
+
+#[cfg(feature = "json")]
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 
 /// Configuration for different logging/tracing options
 /// ===
@@ -76,6 +87,50 @@ fn set_panic_hook() {
     }));
 }
 
+#[cfg(feature = "json")]
+fn bunyan_json_subscriber(nb_output: NonBlocking) {
+    // See https://www.lpalmieri.com/posts/2020-09-27-zero-to-production-4-are-we-observable-yet/#5-7-tracing-bunyan-formatter
+    // Also Bunyan layer addes JSON logging for tracing spans with duration information
+    let formatting_layer = BunyanFormattingLayer::new(config.service_name, nb_output);
+    // The `with` method is provided by `SubscriberExt`, an extension
+    // trait for `Subscriber` exposed by `tracing_subscriber`
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    // `set_global_default` can be used by applications to specify
+    // what subscriber should be used to process spans.
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
+    info!("Enabling JSON and span logging");
+}
+
+#[cfg(feature = "jaeger")]
+fn jaeger_subscriber<S>(config: &TelemetryConfig, subscriber: S)
+where
+    S: tracing::Subscriber
+        + Send
+        + Sync
+        + for<'span> tracing_subscriber::registry::LookupSpan<'span>,
+{
+    // Install a tracer to send traces to Jaeger.  Batching for better performance.
+    let tracer = opentelemetry_jaeger::new_pipeline()
+        .with_service_name(&config.service_name)
+        .with_max_packet_size(9216) // Default max UDP packet size on OSX
+        .with_auto_split_batch(true) // Auto split batches so they fit under packet size
+        .install_batch(opentelemetry::runtime::Tokio)
+        .expect("Could not create async Tracer");
+
+    // Create a tracing subscriber with the configured tracer
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    // Enable Trace Contexts for tying spans together
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    set_global_default(subscriber.with(telemetry)).expect("Failed to set subscriber");
+    info!("Jaeger tracing initialized");
+}
+
 /// Initialize telemetry subscribers based on TelemetryConfig
 /// NOTE: You must keep the returned guard and not drop it until the end of the program, otherwise
 /// logs will not appear!!
@@ -86,22 +141,15 @@ pub fn init(config: TelemetryConfig) -> WorkerGuard {
     let (nb_output, guard) = get_output(&config);
 
     if config.json_log_output {
-        // See https://www.lpalmieri.com/posts/2020-09-27-zero-to-production-4-are-we-observable-yet/#5-7-tracing-bunyan-formatter
-        // Also Bunyan layer addes JSON logging for tracing spans with duration information
-        let formatting_layer = BunyanFormattingLayer::new(config.service_name, nb_output);
-        // The `with` method is provided by `SubscriberExt`, an extension
-        // trait for `Subscriber` exposed by `tracing_subscriber`
-        let subscriber = Registry::default()
-            .with(env_filter)
-            .with(JsonStorageLayer)
-            .with(formatting_layer);
-        // `set_global_default` can be used by applications to specify
-        // what subscriber should be used to process spans.
-        set_global_default(subscriber).expect("Failed to set subscriber");
-
-        info!("Enabling JSON and span logging");
+        #[cfg(feature = "json")]
+        bunyan_json_subscriber(nb_output);
+        #[cfg(not(feature = "json"))]
+        panic!("Cannot enable JSON log output because json package feature is not enabled");
     } else if config.tokio_console {
+        #[cfg(feature = "tokio-console")]
         console_subscriber::init();
+        #[cfg(not(feature = "tokio-console"))]
+        panic!("Cannot enable Tokio console subscriber because tokio-console feature not enabled");
     } else {
         // Output to file or to stdout with ANSI colors
         let fmt_layer = fmt::layer()
@@ -112,22 +160,10 @@ pub fn init(config: TelemetryConfig) -> WorkerGuard {
         let subscriber = Registry::default().with(env_filter).with(fmt_layer);
 
         if config.enable_tracing {
-            // Install a tracer to send traces to Jaeger.  Batching for better performance.
-            let tracer = opentelemetry_jaeger::new_pipeline()
-                .with_service_name(&config.service_name)
-                .with_max_packet_size(9216) // Default max UDP packet size on OSX
-                .with_auto_split_batch(true) // Auto split batches so they fit under packet size
-                .install_batch(opentelemetry::runtime::Tokio)
-                .expect("Could not create async Tracer");
-
-            // Create a tracing subscriber with the configured tracer
-            let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-
-            // Enable Trace Contexts for tying spans together
-            global::set_text_map_propagator(TraceContextPropagator::new());
-
-            set_global_default(subscriber.with(telemetry)).expect("Failed to set subscriber");
-            info!("Jaeger tracing initialized");
+            #[cfg(feature = "jaeger")]
+            jaeger_subscriber(&config, subscriber);
+            #[cfg(not(feature = "jaeger"))]
+            panic!("Cannot enable Jaeger subscriber because jaeger feature not enabled in package");
         } else {
             set_global_default(subscriber).expect("Failed to set subscriber");
         }
@@ -140,6 +176,7 @@ pub fn init(config: TelemetryConfig) -> WorkerGuard {
     guard
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
     use tracing::{debug, info, warn};
